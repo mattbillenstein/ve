f4f6b56bcc461e03a7512de023e099dc52094ae5
Matt Billenstein <matt@vazor.com>
2015-06-21 00:18:29 -0700

    Downgrade memcached to fix osx build failure

--- pkgs/memcached.sh
+++ pkgs/memcached.sh
@@ -1,8 +1,7 @@
-MEMCACHED_VERSION="1.4.24"
+MEMCACHED_VERSION="1.4.23"
 getpkg http://www.memcached.org/files/memcached-${MEMCACHED_VERSION}.tar.gz
 tar zxf memcached-${MEMCACHED_VERSION}.tar.gz
 cd memcached-${MEMCACHED_VERSION}
 ./configure --prefix=$VENV
 $PMAKE
 make install
-

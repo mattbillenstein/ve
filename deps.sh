7ec738778bc5719c4615faf885f899421280c709
Matt Billenstein <matt@vazor.com>
2015-06-21 17:54:09 -0700

    Qualify script paths to deps.sh root

--- deps.sh
+++ deps.sh
@@ -2,18 +2,22 @@
 
 set -e
 
-source ./config.sh
+pushd $(dirname $0) > /dev/null
+SCRIPTPATH="$(pwd)"
+popd > /dev/null
+
+source $SCRIPTPATH/config.sh
 
 sudo mkdir -p $LOG_DIR $RUN_DIR
 sudo chown -R $USER:$GROUP $LOG_DIR $RUN_DIR
 
 if [ "$MOS" == "OSX" ]; then
 
-source os/osx/fink.sh
+source $SCRIPTPATH/os/osx/fink.sh
 
 elif [ "$MOS" == "Ubuntu" ]; then
 
-source os/ubuntu/apt.sh
+source $SCRIPTPATH/os/ubuntu/apt.sh
 
 sudo bash -c "echo $VENV/lib > /etc/ld.so.conf.d/venv.conf"
 sudo ldconfig

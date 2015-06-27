dfd00ef52856bd97afe514118bce86231384f0c6
Matt Billenstein <matt@vazor.com>
2015-06-26 17:24:20 -0700

    mariadb ssl options

--- pkgs/mariadb.sh
+++ pkgs/mariadb.sh
@@ -3,12 +3,16 @@ getpkg https://downloads.mariadb.org/interstitial/mariadb-10.0.20/source/mariadb
 tar zxf mariadb-${MARIADB_VERSION}.tar.gz
 cd mariadb-${MARIADB_VERSION}
 
+# FIXME - this links against the system openssl on OSX instead of the Fink
+# version...
+
 cmake . \
 -DCMAKE_INSTALL_PREFIX=$VENV \
+-DCMAKE_PREFIX_PATH=$VENV \
 -DCMAKE_FIND_FRAMEWORK=LAST \
 -DCMAKE_VERBOSE_MAKEFILE=ON \
 -DMYSQL_DATADIR=/data/mysql \
--DWITH_SSL=yes \
+-DWITH_SSL=system \
 -DDEFAULT_CHARSET=utf8 \
 -DDEFAULT_COLLATION=utf8_general_ci \
 -DWITHOUT_TOKUDB=1 \

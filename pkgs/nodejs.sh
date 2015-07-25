f1083569073c8e45c4020b58a46753d7fcb2f6e1
Matt Billenstein <matt@vazor.com>
2015-07-25 01:04:38 -0700

    A few pkg minor updates

--- pkgs/nodejs.sh
+++ pkgs/nodejs.sh
@@ -1,4 +1,4 @@
-NODEJS_VERSION="0.12.4"
+NODEJS_VERSION="0.12.7"
 
 getpkg http://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}.tar.gz
 tar zxvf node-v${NODEJS_VERSION}.tar.gz
--- pkgs/redis.sh
+++ pkgs/redis.sh
@@ -1,5 +1,5 @@
 # redis
-REDIS_VERSION="3.0.2"
+REDIS_VERSION="3.0.3"
 getpkg http://download.redis.io/releases/redis-${REDIS_VERSION}.tar.gz
 tar zxf redis-${REDIS_VERSION}.tar.gz
 cd redis-${REDIS_VERSION}

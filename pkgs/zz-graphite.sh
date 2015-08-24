e461aebb95e4cb9d78220553eed130c1e5e5e13f
Matt Billenstein <matt@vazor.com>
2015-08-24 01:30:29 -0700

    Graphite fixes

--- pkgs/zz-graphite.sh
+++ pkgs/zz-graphite.sh
@@ -12,7 +12,7 @@ $PIP_INSTALL 'twisted<=12.0'
 $PIP_INSTALL 'django<1.7'
 $PIP_INSTALL --allow-external pycairo --allow-unverified pycairo 'pycairo==1.8.8'
 $PIP_INSTALL carbon
-$PIP_INSTALL django-tagging
+$PIP_INSTALL 'django-tagging==0.3.4'
 $PIP_INSTALL git+https://github.com/graphite-project/graphite-web.git
 $PIP_INSTALL pyparsing
 $PIP_INSTALL whisper
@@ -28,9 +28,9 @@ mv $VENV/graphite$VENV/bin/* $VENV/graphite/bin/
 mv $VENV/graphite$VENV/conf/* $VENV/graphite/conf/
 mv $VENV/graphite$VENV/include $VENV/graphite/
 mv $VENV/graphite$VENV/lib $VENV/graphite/
-mv $VENV/graphite/lib/python2.7/site-packages/opt/graphite/lib/carbon $VENV/graphite/lib/python2.7/
+mv $VENV/graphite/lib/python2.7/site-packages/opt/graphite/lib/carbon $VENV/graphite/lib/python2.7/site-packages/
 mv $VENV/graphite/lib/python2.7/site-packages/opt/graphite/lib/twisted/plugins/* $VENV/graphite/lib/python2.7/site-packages/twisted/plugins/
-rm -fR $$VENV/graphite/lib/python2.7/site-packages/opt
+rm -fR $VENV/graphite/lib/python2.7/site-packages/opt
 rm -fR $VENV/graphite$VENV $VENV$VENV $VENV/graphite/storage $VENV/graphite/examples
 
 # WTF this module is broken

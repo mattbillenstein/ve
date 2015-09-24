a0de48cbdbc3c75ee39cabc2e4d532c1e6a59c8f
Matt Billenstein <matt@vazor.com>
2015-09-23 23:57:40 -0700

    Doh, remove old download

--- pkgs/pgbouncer.sh
+++ pkgs/pgbouncer.sh
@@ -1,6 +1,5 @@
 PGBOUNCER_VERSION="1.6.1"
 getpkg http://pgbouncer.github.io/downloads/files/${PGBOUNCER_VERSION}/pgbouncer-${PGBOUNCER_VERSION}.tar.gz
-getpkg https://pgbouncer.github.io/downloads/pgbouncer-${PGBOUNCER_VERSION}.tar.gz
 tar zxvf pgbouncer-${PGBOUNCER_VERSION}.tar.gz
 cd pgbouncer-${PGBOUNCER_VERSION}
 ./configure --prefix=$VENV

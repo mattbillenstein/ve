dea9f65d719a030d3b9d534aada5db9cc56cf059
Matt Billenstein <matt@vazor.com>
2015-09-23 23:33:03 -0700

    Fixup golang darwin pkg

--- pkgs/golang.sh
+++ pkgs/golang.sh
@@ -1,16 +1,15 @@
 GOLANG_VERSION="1.5.1"
 GOLANG_KERNEL="linux"
-GOLANG_PLATFORM=""
 
-# https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
-# https://storage.googleapis.com/golang/go1.4.2.darwin-amd64-osx10.8.tar.gz
+# https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
+# https://storage.googleapis.com/golang/go1.5.1.darwin-amd64.tar.gz
+
 
 if [ "$MOS" == "OSX" ]; then
 GOLANG_KERNEL="darwin"
-GOLANG_PLATFORM="-osx10.8"
 fi
 
-NAME="go${GOLANG_VERSION}.${GOLANG_KERNEL}-amd64${GOLANG_PLATFORM}"
+NAME="go${GOLANG_VERSION}.${GOLANG_KERNEL}-amd64"
 
 getpkg https://storage.googleapis.com/golang/${NAME}.tar.gz
 tar zxf ${NAME}.tar.gz

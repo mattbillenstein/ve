4d20eb51d2f67b817dece1ae827de28f2074a79e
Matt Billenstein <matt@vazor.com>
2015-10-01 13:10:28 -0700

    No verbose untar

--- pkgs/available/nodejs.sh
+++ pkgs/available/nodejs.sh
@@ -1,7 +1,7 @@
 NODEJS_VERSION="0.12.7"
 
 getpkg http://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}.tar.gz
-tar zxvf node-v${NODEJS_VERSION}.tar.gz
+tar zxf node-v${NODEJS_VERSION}.tar.gz
 cd node-v${NODEJS_VERSION}/
 ./configure --prefix=$VENV
 $PMAKE install

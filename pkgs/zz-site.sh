23aab942d53bfaac513f0c6559ac982440004160
Matt Billenstein <matt@vazor.com>
2015-11-02 16:57:50 -0800

    Add flyway command-line

--- pkgs/zz-site.sh
+++ pkgs/zz-site.sh
@@ -7,3 +7,8 @@ npm install -g grunt-cli
 if [ "$MOS" == "Ubuntu" ]; then
 sudo apt-get -y install fortune cowsay
 fi
+
+getpkg http://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/3.2.1/flyway-commandline-3.2.1.tar.gz
+tar zxf flyway-commandline-3.2.1.tar.gz
+mv flyway-3.2.1 $VENV/flyway
+chmod 755 $VENV/flyway/flyway

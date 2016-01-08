c4b949e03c0d89fcf94e9e2f58b4d405aafee7a9
Matt Billenstein <matt@vazor.com>
2016-01-08 12:47:54 -0800

    Update android-wait-for-emulator

--- pkgs/zz-site.sh
+++ pkgs/zz-site.sh
@@ -45,9 +45,10 @@ timeout_in_sec=360
 
 until [[ "\$bootanim" =~ "stopped" ]]; do
   bootanim="\$(/ave/android-sdk/platform-tools/adb -e shell getprop init.svc.bootanim 2>&1)"
-  if [[ "\$bootanim" =~ "device not found" || "\$bootanim" =~ "device offline" || "\$bootanim" =~ "running" ]]; then
+  if [[ "\$bootanim" =~ "device not found" || "\$bootanim" =~ "device offline"
+    || "\$bootanim" =~ "running" || "\$bootanim" =~ "error" ]]; then
     let "failcounter += 1"
-    echo "Waiting for emulator to start"
+    echo "Waiting for emulator to start \$failcounter"
     if [[ \$failcounter -gt timeout_in_sec ]]; then
       echo "Timeout (\$timeout_in_sec seconds) reached; failed to start emulator"
       exit 1

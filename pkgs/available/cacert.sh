fdfc691238d6e131aa357a1a8627d45d79aec0c6
Matt Billenstein <matt@vazor.com>
2016-02-05 23:06:25 -0800

    Use mozilla ca cert bundle

--- pkgs/available/cacert.sh
+++ pkgs/available/cacert.sh
@@ -1,9 +1,16 @@
 
 # so many different packages include their own out of date bundles -- replace
-# them all with the curl bundle
+# them all with the one mozilla uses using a script from the curl source
 
 cd $BUILD_DIR
-curl -O http://curl.haxx.se/ca/cacert.pem
+
+#curl -O http://curl.haxx.se/ca/cacert.pem
+git clone https://github.com/bagder/curl.git
+cd curl/lib
+./mk-ca-bundle.pl
+cp ca-bundle.crt $BUILD_DIR/cacert.pem
+
+cd $BUILD_DIR
 for target in $(find $VENV -name 'cacert.pem' | grep -v $VENV/build/cacert.pem); do
 cp cacert.pem $target
 done

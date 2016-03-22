2a47638acdbc42735f0ca05a026707584e6386d4
Matt Billenstein <matt@vazor.com>
2016-03-22 11:50:47 -0700

    Update nginx upstream check module

--- pkgs/available/nginx.sh
+++ pkgs/available/nginx.sh
@@ -1,7 +1,5 @@
 NGINX_VERSION="1.8.1"
 
-WD="$PWD"
-
 rm -fR nginx-${NGINX_VERSION}* ngx_*
 
 getpkg https://github.com/simpl/ngx_devel_kit/archive/v0.2.19.tar.gz
@@ -10,14 +8,20 @@ tar zxf v0.2.19.tar.gz
 getpkg https://github.com/openresty/lua-nginx-module/archive/v0.9.17.tar.gz
 tar zxf v0.9.17.tar.gz
 
-getpkg https://github.com/yaoweibin/nginx_upstream_check_module/archive/v0.3.0.tar.gz
-tar zxf v0.3.0.tar.gz
+# Need a newer release for https://github.com/yaoweibin/nginx_upstream_check_module/pull/48
+#getpkg https://github.com/yaoweibin/nginx_upstream_check_module/archive/v0.3.0.tar.gz
+#tar zxf v0.3.0.tar.gz
+git clone https://github.com/yaoweibin/nginx_upstream_check_module.git
+cd nginx_upstream_check_module
+git checkout 10782eaff5
+cd $BUILD_DIR
 
 getpkg http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 tar zxf nginx-${NGINX_VERSION}.tar.gz
 cd nginx-${NGINX_VERSION}
 
-patch -p1 < $WD/nginx_upstream_check_module-0.3.0/check_1.7.2+.patch
+#patch -p1 < $BUILD_DIR/nginx_upstream_check_module-0.3.0/check_1.7.2+.patch
+patch -p1 < $BUILD_DIR/nginx_upstream_check_module/check_1.7.5+.patch
 
 export LUAJIT_LIB="$VENV/lib"
 export LUAJIT_INC="$VENV/include/luajit-2.0"
@@ -36,9 +40,9 @@ export LUAJIT_INC="$VENV/include/luajit-2.0"
 --lock-path=$RUN_DIR/nginx/nginx.lock \
 --with-cc-opt="$CFLAGS" \
 --with-ld-opt="$LDFLAGS -Wl,-rpath,$VENV/lib" \
---add-module=$WD/ngx_devel_kit-0.2.19 \
---add-module=$WD/lua-nginx-module-0.9.17 \
---add-module=$WD/nginx_upstream_check_module-0.3.0
+--add-module=$BUILD_DIR/ngx_devel_kit-0.2.19 \
+--add-module=$BUILD_DIR/lua-nginx-module-0.9.17 \
+--add-module=$BUILD_DIR/nginx_upstream_check_module
 
 $PMAKE
 make install

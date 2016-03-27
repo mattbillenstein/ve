d5afa33e19e30e0d325f54c141ad4e24f4e84272
Matt Billenstein <matt@vazor.com>
2016-03-27 13:02:37 -0700

    Add phantomjs

---
+++ pkgs/available/phantomjs.sh
@@ -0,0 +1,11 @@
+PHANTOMJS_VERSION="1.9.8"
+
+if [ "$MOS" = "OSX" ]; then
+getpkg https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-macosx.zip
+unzip phantomjs-${PHANTOMJS_VERSION}-macosx.zip
+mv phantomjs-${PHANTOMJS_VERSION}-macosx/bin/* $VENV/bin/
+else
+getpkg https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-x86_64.tar.bz2
+tar jxf phantomjs-${PHANTOMJS_VERSION}-linux-x86_64.tar.bz2
+mv phantomjs-${PHANTOMJS_VERSION}-linux-x86_64/bin/* $VENV/bin/
+fi

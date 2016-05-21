f436e54ce9b083704cedec02e5334a23ff5dd4c6
Matt Billenstein <matt@vazor.com>
2016-05-20 19:27:27 -0700

    Bump postgres to 9.5.3 -- add a couple more extensions

--- pkgs/available/postgresql.sh
+++ pkgs/available/postgresql.sh
@@ -1,4 +1,4 @@
-POSTGRES_VERSION="9.4.6"
+POSTGRES_VERSION="9.5.3"
 getpkg http://ftp.postgresql.org/pub/source/v${POSTGRES_VERSION}/postgresql-${POSTGRES_VERSION}.tar.bz2
 tar jxf postgresql-${POSTGRES_VERSION}.tar.bz2
 cd postgresql-${POSTGRES_VERSION}
@@ -6,10 +6,11 @@ cd postgresql-${POSTGRES_VERSION}
 # hack default socket dir
 sed -i -e 's:DEFAULT_PGSOCKET_DIR[ ][ ]*"/tmp":DEFAULT_PGSOCKET_DIR "/data/run/pg":' src/include/pg_config_manual.h
 
-./configure --prefix=$VENV --with-openssl
+./configure --prefix=$VENV --with-openssl --with-perl
 $PMAKE
 make install
-for ext in pgcrypto hstore; do
+
+for ext in pgcrypto hstore cube; do
 cd contrib/$ext
 $PMAKE
 make install

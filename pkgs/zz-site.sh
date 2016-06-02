e92ce47619b1e6f5e4bec1ec0ee06410934fb507
Matt Billenstein <matt@vazor.com>
2016-06-02 12:24:43 -0700

    Add some pypy pkgs

--- pkgs/zz-site.sh
+++ pkgs/zz-site.sh
@@ -1,2 +1,11 @@
 # This is the last package built -- add your dependencies here,
 # python/ruby/node pkgs, etc.
+
+$VENV/pypy/bin/pip install \
+boto \
+boto3 \
+google-api-python-client \
+psycopg2cffi \
+pymongo \
+requests \
+rollbar

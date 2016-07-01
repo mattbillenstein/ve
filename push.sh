b08ecea789e639a5c8bc919aff9868c6deb40ba7
Yinmeng Zhang <yinmeng@apptimize.com>
2016-06-30 17:32:07 -0700

    Add a dry run option to push.sh

--- push.sh
+++ push.sh
@@ -6,4 +6,10 @@ popd > /dev/null
 
 source $SCRIPTPATH/config.sh
 
-rsync -av --delete --bwlimit=1500 $* $VENV/ $RSYNC_USER@$RSYNC_HOST:$RSYNC_PATH/$OS
+if [[ $1 == "-n" ]]; then
+  echo "Dry Run"
+  rsync --dry-run -av --delete --bwlimit=1500 $* $VENV/ $RSYNC_USER@$RSYNC_HOST:$RSYNC_PATH/$OS
+else
+  echo "Real Thing"
+  rsync -av --delete --bwlimit=1500 $* $VENV/ $RSYNC_USER@$RSYNC_HOST:$RSYNC_PATH/$OS
+fi

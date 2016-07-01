e4e4bce7707db75576d11b59393cca10a91bf083
Matt Billenstein <matt@vazor.com>
2016-07-01 12:33:50 -0700

    Revert "Add a dry run option to push.sh"
    This reverts commit b08ecea789e639a5c8bc919aff9868c6deb40ba7.

--- push.sh
+++ push.sh
@@ -6,10 +6,4 @@ popd > /dev/null
 
 source $SCRIPTPATH/config.sh
 
-if [[ $1 == "-n" ]]; then
-  echo "Dry Run"
-  rsync --dry-run -av --delete --bwlimit=1500 $* $VENV/ $RSYNC_USER@$RSYNC_HOST:$RSYNC_PATH/$OS
-else
-  echo "Real Thing"
-  rsync -av --delete --bwlimit=1500 $* $VENV/ $RSYNC_USER@$RSYNC_HOST:$RSYNC_PATH/$OS
-fi
+rsync -av --delete --bwlimit=1500 $* $VENV/ $RSYNC_USER@$RSYNC_HOST:$RSYNC_PATH/$OS

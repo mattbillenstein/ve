bb77dcabac5b45de8c8c93f4f88820087ca36d22
Matt Billenstein <matt@vazor.com>
2016-10-18 19:19:11 -0700

    add fluent s3 gem

--- pkgs/available/fluentd.sh
+++ pkgs/available/fluentd.sh
@@ -1,3 +1,4 @@
 /tve/bin/gem install fluentd
 #/tve/bin/gem install fluent-plugin-secure-forward
 /tve/bin/gem specific_install -l https://github.com/mattbillenstein/fluent-plugin-secure-forward.git
+/tve/bin/fluent-gem install fluent-plugin-s3

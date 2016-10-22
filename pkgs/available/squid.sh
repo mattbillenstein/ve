5c92bcdd64518644b3da1d29221cce74d3af8e72
Matt Billenstein <matt@vazor.com>
2016-10-21 21:16:09 -0700

    Tweak squid build

--- pkgs/available/squid.sh
+++ pkgs/available/squid.sh
@@ -1,6 +1,6 @@
 getpkg http://www.squid-cache.org/Versions/v3/3.5/squid-3.5.22.tar.gz
 tar zxvf squid-3.5.22.tar.gz
 cd squid-3.5.22
-./configure --prefix=$VENV --disable-auth --with-krb5-config=no --disable-external-acl-helpers --disable-eui
+./configure --prefix=$VENV --disable-auth --with-krb5-config=no --disable-external-acl-helpers --disable-eui --disable-arch-native
 $PMAKE
 make install

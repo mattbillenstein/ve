b0311fa578a1a3dd1087f8eef84adeaf52c03da8
Matt Billenstein <matt@vazor.com>
2016-10-24 15:24:56 -0700

    Disable a bunch of squid stuff so we don't link external libs we don't really need

--- pkgs/available/squid.sh
+++ pkgs/available/squid.sh
@@ -1,6 +1,6 @@
 getpkg http://www.squid-cache.org/Versions/v3/3.5/squid-3.5.22.tar.gz
 tar zxvf squid-3.5.22.tar.gz
 cd squid-3.5.22
-./configure --prefix=$VENV --disable-auth --with-krb5-config=no --disable-external-acl-helpers --disable-eui --disable-arch-native
+./configure --prefix=$VENV --disable-auth --with-krb5-config=no --disable-external-acl-helpers --disable-eui --disable-arch-native --without-expat --without-libxml2 --without-gnutls --without-mit-krb5 --without-heimdal-krb5 --without-gnugss --without-netfilter-conntrack --without-libcap --without-nettle
 $PMAKE
 make install

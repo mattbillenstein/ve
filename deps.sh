b9c90d005ccc5079417d3aab48ee5c437d9c228a
Matt Billenstein <matt@vazor.com>
2016-10-27 16:53:53 -0700

    Perhaps, we can hope, the certificate store isn't crap anymore...

--- deps.sh
+++ deps.sh
@@ -12,9 +12,6 @@ if [ "$MOS" == "OSX" ]; then
 
 source $SCRIPTPATH/os/osx/fink.sh
 
-# put our own cafile in place
-sudo cp $VENV/lib/python2.7/site-packages/certifi/cacert.pem $($VENV/bin/python -c 'import ssl;print ssl.get_default_verify_paths().openssl_cafile')
-
 elif [ "$MOS" == "Ubuntu" ]; then
 
 source $SCRIPTPATH/os/ubuntu/apt.sh

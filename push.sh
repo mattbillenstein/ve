04f67b8707fb136816d47001cba00eae8b34a899
Matt Billenstein <matt@vazor.com>
2017-01-06 11:55:10 -0800

    git gc on push

--- push.sh
+++ push.sh
@@ -8,6 +8,7 @@ source $SCRIPTPATH/config.sh
 
 # re-package the source on every push
 rm -fR $VENV/src
+git gc --aggressive
 cp -a $SCRIPTPATH $VENV/src
 
 rsync -av --delete --delete-excluded --exclude '*.swp' --bwlimit=1500 $* $VENV/ $RSYNC_USER@$RSYNC_HOST:$RSYNC_PATH/$OS

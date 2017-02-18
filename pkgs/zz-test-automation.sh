a6fabb5c98170b8a978437a95e971d3df6158083
Chris Joe <chris@apptimize.com>
2017-02-17 18:34:18 -0800

    RET-1345: Update ave to support Xcode 8 and iOS 10

--- pkgs/zz-test-automation.sh
+++ pkgs/zz-test-automation.sh
@@ -1,19 +1,38 @@
 # test automation tooling
 if [ "$MOS" == "OSX" ]; then
-getpkg http://chromedriver.storage.googleapis.com/2.25/chromedriver_mac64.zip
+getpkg http://chromedriver.storage.googleapis.com/2.27/chromedriver_mac64.zip
 unzip chromedriver_mac64.zip
 mv chromedriver $VENV/bin/
 else
-getpkg http://chromedriver.storage.googleapis.com/2.25/chromedriver_linux64.zip
+getpkg http://chromedriver.storage.googleapis.com/2.27/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 mv chromedriver $VENV/bin/
 fi
+
+# install homebrew
+$VENV/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+BREW="/usr/local/bin/brew"
+$BREW install libimobiledevice --HEAD  # install from HEAD to get important updates
+$BREW install ideviceinstaller         # only works for ios 9. for ios 10, see below
+$BREW install carthage
+
+# appium requirements
 $VENV/bin/npm install -g appium  # this does not install cleanly on arch...
+$VENV/bin/npm install -g authorize-ios
+$VENV/bin/npm install -g ios-deploy
+$VENV/bin/gem install xcpretty
+
+# pip install packages for automation support
 $VENV/bin/pip install Appium-Python-Client
 $VENV/bin/pip install enum34
 $VENV/bin/pip install py
 $VENV/bin/pip install pytest
 $VENV/bin/pip install selenium
+$VENV/bin/pip install subprocess32
+
+# sym-link iproxy since it's the incorrect version
+sudo mv /sw/bin/iproxy /sw/bin/iproxy_old
+sudo ln -s /ave/bin/iproxy /sw/bin/iproxy
 
 if [ "$MOS" == "OSX" ]; then
 cd $BUILD_DIR

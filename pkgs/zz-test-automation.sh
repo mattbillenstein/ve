1f187cc253aaffb6ca82c99c14b2299ff0282ee4
Chris Joe <chris@apptimize.com>
2017-02-21 15:17:12 -0800

    Ensure the iOS Automation tools only get installed on macOS.

--- pkgs/zz-test-automation.sh
+++ pkgs/zz-test-automation.sh
@@ -10,17 +10,21 @@ mv chromedriver $VENV/bin/
 fi
 
 # install homebrew
+if [ "$MOS" == "OSX" ]; then
 $VENV/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 BREW="/usr/local/bin/brew"
 $BREW install libimobiledevice --HEAD  # install from HEAD to get important updates
 $BREW install ideviceinstaller         # only works for ios 9. for ios 10, see below
 $BREW install carthage
+fi
 
 # appium requirements
 $VENV/bin/npm install -g appium  # this does not install cleanly on arch...
+if [ "$MOS" == "OSX" ]; then
 $VENV/bin/npm install -g authorize-ios
 $VENV/bin/npm install -g ios-deploy
 $VENV/bin/gem install xcpretty
+fi
 
 # pip install packages for automation support
 $VENV/bin/pip install Appium-Python-Client
@@ -31,18 +35,7 @@ $VENV/bin/pip install selenium
 $VENV/bin/pip install subprocess32
 
 # sym-link iproxy since it's the incorrect version
+if [ "$MOS" == "OSX" ]; then
 sudo mv /sw/bin/iproxy /sw/bin/iproxy_old
 sudo ln -s /ave/bin/iproxy /sw/bin/iproxy
-
-if [ "$MOS" == "OSX" ]; then
-cd $BUILD_DIR
-# idevice installer
-for PKG in libplist libusbmuxd libimobiledevice ideviceinstaller; do
-rm -fR $PKG
-git clone https://github.com/libimobiledevice/$PKG.git
-cd $PKG
-./autogen.sh --prefix=$VENV
-$PMAKE install
-cd ..
-done
 fi

27398d628870403774fa8404e67eb8b0fcc79281
Matt Billenstein <matt@vazor.com>
2017-04-20 18:19:29 -0700

    Pin fluent versions

--- pkgs/available/fluentd.sh
+++ pkgs/available/fluentd.sh
@@ -1,4 +1,4 @@
-$VENV/bin/gem install fluentd
+$VENV/bin/gem install fluentd -v 0.14.8
 #$VENV/bin/gem install fluent-plugin-secure-forward
 $VENV/bin/gem specific_install -l https://github.com/mattbillenstein/fluent-plugin-secure-forward.git
-$VENV/bin/fluent-gem install fluent-plugin-s3
+$VENV/bin/fluent-gem install fluent-plugin-s3 -v 0.7.1

c5858ad6d18179f823d7564c7a2631d089e97a8e
Matt Billenstein <matt@vazor.com>
2017-06-23 20:57:28 +0200

    Remove SSL_CERT_FILE on osx

--- pkgs/available/scripts.sh
+++ pkgs/available/scripts.sh
@@ -21,7 +21,6 @@ deactivate () {
     unset GOROOT
     unset AIRFLOW_HOME
     unset NODE_PATH
-    unset SSL_CERT_FILE
 
     # This should detect bash and zsh, which have a hash command that must
     # be called to get it to forget past commands.  Without forgetting
@@ -72,9 +71,6 @@ export PATH
 
 export AIRFLOW_HOME="/data/airflow"
 export NODE_PATH="\$VIRTUAL_ENV/lib/node_modules"
-if [ "\$(uname)" == "Darwin" ]; then
-export SSL_CERT_FILE="\$VIRTUAL_ENV/lib/python2.7/site-packages/certifi/cacert.pem"
-fi
 
 _OLD_VIRTUAL_PYTHONPATH="\$PYTHONPATH"
 

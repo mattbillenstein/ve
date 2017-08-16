9492ca4fb7cb211f49c3434d1262f7da33e113b6
Matt Billenstein <matt@vazor.com>
2017-08-16 01:57:32 -0700

    Update nodejs

--- pkgs/available/nodejs.sh
+++ pkgs/available/nodejs.sh
@@ -1,6 +1,6 @@
-NODEJS_VERSION="5.9.1"
+NODEJS_VERSION="6.11.2"
 
-getpkg http://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}.tar.gz
+getpkg https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}.tar.gz
 tar zxf node-v${NODEJS_VERSION}.tar.gz
 cd node-v${NODEJS_VERSION}/
 ./configure --prefix=$VENV

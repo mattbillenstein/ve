9cdbd8ee6405f507c302f4be0315344c714e4c24
Matt Billenstein <matt@vazor.com>
2017-11-03 13:31:32 -0700

    Update nsq, the old binaries don't run on high sierra

--- pkgs/available/nsq.sh
+++ pkgs/available/nsq.sh
@@ -1,10 +1,10 @@
-NSQ_VERSION="0.3.8"
+NSQ_VERSION="1.0.0-compat"
 
 NSQ_PLATFORM="linux"
 if [ "$MOS" == "OSX" ]; then
 NSQ_PLATFORM="darwin"
 fi
 
-getpkg https://s3.amazonaws.com/bitly-downloads/nsq/nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.6.2.tar.gz
-tar zxf nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.6.2.tar.gz
-mv nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.6.2/bin/* $VENV/bin/
+getpkg https://s3.amazonaws.com/bitly-downloads/nsq/nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.8.tar.gz
+tar zxf nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.8.tar.gz
+mv nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.8/bin/* $VENV/bin/

4ef855f908b1aac212399791a74a5fac21adb91b
Matt Billenstein <matt@vazor.com>
2018-01-31 20:52:54 +0000

    Add golang, build nsq from source to pick up some fixes

--- pkgs/available/golang.sh
+++ pkgs/available/golang.sh
@@ -1,19 +1,22 @@
 GOLANG_VERSION="1.8.3"
 GOLANG_KERNEL="linux"
-
-# http://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
-# http://storage.googleapis.com/golang/go1.5.1.darwin-amd64.tar.gz
-
 if [ "$MOS" == "OSX" ]; then
 GOLANG_KERNEL="darwin"
 fi
 
 NAME="go${GOLANG_VERSION}.${GOLANG_KERNEL}-amd64"
 
-getpkg http://storage.googleapis.com/golang/${NAME}.tar.gz
+getpkg https://dl.google.com/go/${NAME}.tar.gz
 tar zxf ${NAME}.tar.gz
 
 rm -fR $VENV/opt/go
 mkdir -p $VENV/opt
 
 mv go $VENV/opt/
+
+# Dep - pkg management
+getpkg https://github.com/golang/dep/releases/download/v0.4.1/dep-${GOLANG_KERNEL}-amd64 $VENV/opt/go/bin/dep
+chmod 755 $VENV/opt/go/bin/dep
+
+export GOROOT="$VENV/opt/go"
+export GOPATH="$BUILD_DIR/golang"
--- pkgs/available/nsq.sh
+++ pkgs/available/nsq.sh
@@ -1,10 +1,23 @@
-NSQ_VERSION="1.0.0-compat"
+# need to build from source to pick up https://github.com/nsqio/nsq/pull/935
 
-NSQ_PLATFORM="linux"
-if [ "$MOS" == "OSX" ]; then
-NSQ_PLATFORM="darwin"
-fi
+#NSQ_VERSION="1.0.0-compat"
+#
+#NSQ_PLATFORM="linux"
+#if [ "$MOS" == "OSX" ]; then
+#NSQ_PLATFORM="darwin"
+#fi
+#
+#getpkg https://s3.amazonaws.com/bitly-downloads/nsq/nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.8.tar.gz
+#tar zxf nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.8.tar.gz
+#mv nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.8/bin/* $VENV/bin/
 
-getpkg https://s3.amazonaws.com/bitly-downloads/nsq/nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.8.tar.gz
-tar zxf nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.8.tar.gz
-mv nsq-${NSQ_VERSION}.${NSQ_PLATFORM}-amd64.go1.8/bin/* $VENV/bin/
+mkdir -p $GOPATH/src/github.com/nsqio
+cd $GOPATH/src/github.com/nsqio
+git clone https://github.com/nsqio/nsq.git
+cd nsq
+git checkout 2305c6fd2320e32442ac741c634791c21b8734b2
+
+$VENV/opt/go/bin/dep ensure
+PATH="$VENV/opt/go/bin:$PATH" make -j4
+rm -f $VENV/bin/nsq* $VENV/bin/to_nsq
+cp build/* $VENV/bin/

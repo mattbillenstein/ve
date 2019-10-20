33d137632ffbfc9e4eb8822a51430e7c9aa3c9ce
Matt Billenstein <matt@vazor.com>
2019-10-19 20:46:03 -0700

    Remove golang dep

--- pkgs/available/golang.sh
+++ pkgs/available/golang.sh
@@ -14,10 +14,5 @@ mkdir -p $VENV/opt
 
 mv go $VENV/opt/
 
-# Dep - pkg management
-getpkg https://github.com/golang/dep/releases/download/v0.5.0/dep-${GOLANG_KERNEL}-amd64
-cp dep-${GOLANG_KERNEL}-amd64 $VENV/opt/go/bin/dep
-chmod 755 $VENV/opt/go/bin/dep
-
 export GOROOT="$VENV/opt/go"
 export GOPATH="$BUILD_DIR/golang"

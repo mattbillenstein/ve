74a2dfef8ff42097d019454d5d60dfbef756455a
Matt Billenstein <matt@vazor.com>
2020-03-28 00:17:23 +0000

    Remove -Werror for some deprecations on macos

--- pkgs/available/squid.sh
+++ pkgs/available/squid.sh
@@ -4,6 +4,9 @@ SQUID_SHA256SUM="df0685e851a1b68eede3790423986e30f4e162e9f7cd1cf92dbea65177201c8
 getpkg http://www.squid-cache.org/Versions/v4/squid-${SQUID_VERSION}.tar.gz $SQUID_SHA256SUM
 tar zxf squid-${SQUID_VERSION}.tar.gz
 cd squid-${SQUID_VERSION}
-./configure --prefix=$VENV --disable-auth --with-krb5-config=no --disable-external-acl-helpers --disable-eui --disable-arch-native --without-expat --without-libxml2 --without-gnutls --without-mit-krb5 --without-heimdal-krb5 --without-gnugss --without-netfilter-conntrack --without-libcap --without-nettle
+./configure --prefix=$VENV --disable-auth --with-krb5-config=no --disable-external-acl-helpers --disable-eui --disable-arch-native --without-expat --without-libxml2 --without-gnutls --without-mit-krb5 --without-heimdal-krb5 --without-gnugss --without-netfilter-conntrack --without-libcap --without-nettle \
+    --disable-strict-error-checking
+    # above fix re deprecated apis on macos 3/2020
+
 $PMAKE
 make install

8b2927d056362da96b42d963dec8329c08c90cc5
Matt Billenstein <matt@vazor.com>
2020-10-22 21:38:52 +0000

    Fix golang download

--- pkgs/available/golang.sh
+++ pkgs/available/golang.sh
@@ -1,14 +1,14 @@
-GOLANG_VERSION="1.12.9"
+GOLANG_VERSION="1.15.3"
 GOLANG_KERNEL="linux"
-GOLANG_SHA256SUM="ac2a6efcc1f5ec8bdc0db0a988bb1d301d64b6d61b7e8d9e42f662fbb75a2b9b"
+GOLANG_SHA256SUM="010a88df924a81ec21b293b5da8f9b11c176d27c0ee3962dc1738d2352d3c02d"
 if [ "$MOS" == "MacOS" ]; then
 GOLANG_KERNEL="darwin"
-GOLANG_SHA256SUM="4f189102b15de0be1852d03a764acb7ac5ea2c67672a6ad3a340bd18d0e04bb4"
+GOLANG_SHA256SUM="d9fedc1f579fa60ee9efffdeed718d9a1a40f02ca1cb8640d8d01d7f472d4a56"
 fi
 
 NAME="go${GOLANG_VERSION}.${GOLANG_KERNEL}-amd64"
 
-getpkg https://dl.google.com/go/${NAME}.tar.gz $GOLANG_SHA256SUM
+getpkg https://golang.org/dl/${NAME}.tar.gz $GOLANG_SHA256SUM
 tar zxf ${NAME}.tar.gz
 
 rm -fR $VENV/opt/go

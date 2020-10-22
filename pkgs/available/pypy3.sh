514a0dd8600b4e107203acc0fbfdce5452f55f4d
Matt Billenstein <matt@vazor.com>
2020-10-22 21:20:31 +0000

    pypy url fixes

--- pkgs/available/pypy3.sh
+++ pkgs/available/pypy3.sh
@@ -1,5 +1,5 @@
-PYPY_VERSION="v7.3.0"
-PYPY_SHA256SUM="d3d549e8f43de820ac3385b698b83fa59b4d7dd6cf3fe34c115f731e26ad8856"
+PYPY_VERSION="v7.3.2"
+PYPY_SHA256SUM="a285ddcbc909d68c648585fae4f33b0ba24961bb4e8fafe5874cf725d6e83df6"
 PYPY_ARCH="linux64"
 
 rm -fR $VENV/opt/pypy3
@@ -7,26 +7,26 @@ mkdir -p $VENV/opt
 
 if [ "$MOS" == "MacOS" ]; then
 PYPY_ARCH="osx64"
-PYPY_SHA256SUM="87b2545dad75fe3027b4b2108aceb9fdadcdd24e61ae312ac48b449fdd452bf3"
+PYPY_SHA256SUM="337dd4d9e529d2f221e0beb092236c18430e0564ab835c6bba425a1daf7c9958"
 fi
 
 if [ "$MOS" != "Arch" ]; then
-getpkg https://bitbucket.org/pypy/pypy/downloads/pypy3.6-${PYPY_VERSION}-${PYPY_ARCH}.tar.bz2 $PYPY_SHA256SUM
-tar jxf pypy3.6-${PYPY_VERSION}-${PYPY_ARCH}.tar.bz2
-mv pypy3.6-${PYPY_VERSION}-${PYPY_ARCH} $VENV/opt/pypy3
+getpkg https://downloads.python.org/pypy/pypy3.7-${PYPY_VERSION}-${PYPY_ARCH}.tar.bz2 $PYPY_SHA256SUM
+tar jxf pypy3.7-${PYPY_VERSION}-${PYPY_ARCH}.tar.bz2
+mv pypy3.7-${PYPY_VERSION}-${PYPY_ARCH} $VENV/opt/pypy3
 else
 # the official binaries are built on Ubuntu and MacOS -- on other distros, build
 # from source; it's best to actually install pypy on those platforms so the
 # build doesn't take as long
 rm -fR /tmp/usession-release-*
 PYPY_SHA256SUM=""
-getpkg https://bitbucket.org/pypy/pypy/downloads/pypy3.6-${PYPY_VERSION}-src.tar.bz2 $PYPY_SHA256SUM
-tar jxf pypy3.6-${PYPY_VERSION}-src.tar.bz2
-cd pypy3.6-${PYPY_VERSION}-src/pypy/goal
+getpkg https://downloads.python.org/pypy/pypy3.7-${PYPY_VERSION}-src.tar.bz2 $PYPY_SHA256SUM
+tar jxf pypy3.7-${PYPY_VERSION}-src.tar.bz2
+cd pypy3.7-${PYPY_VERSION}-src/pypy/goal
 pypy ../../rpython/bin/rpython --opt=jit
 PYTHONPATH=../.. ./pypy-c ../../pypy/tool/build_cffi_imports.py
 cd ../tool/release
-$VENV/bin/python ./package.py pypy3.6-${PYPY_VERSION}-${PYPY_ARCH}
+$VENV/bin/python ./package.py pypy3.7-${PYPY_VERSION}-${PYPY_ARCH}
 mv /tmp/usession-release-7.0-1/build/pypy-nightly $VENV/opt/pypy3
 fi
 

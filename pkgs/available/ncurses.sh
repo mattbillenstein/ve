10461ae25d047682e63f5b3bf568d6788624bd92
Matt Billenstein <matt@vazor.com>
2020-10-23 14:00:29 -0700

    Fix order of lib builds re dependencies

---
+++ pkgs/available/ncurses.sh
@@ -0,0 +1,11 @@
+NCURSES_VERSION="6.2"
+NCURSES_SHA256SUM="30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227f7da5b71cbea86c9d"
+
+rm -fR ncurses*
+getpkg https://invisible-mirror.net/archives/ncurses/ncurses-${NCURSES_VERSION}.tar.gz $NCURSES_SHA256SUM
+tar zxvf ncurses-${NCURSES_VERSION}.tar.gz 
+cd ncurses-${NCURSES_VERSION}
+
+./configure --prefix=$VENV
+$PMAKE
+make install

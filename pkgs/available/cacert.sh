146827af47b15a5e45b00b79080775de77a85b30
Matt Billenstein <matt@vazor.com>
2022-07-08 03:17:01 +0000

    Fix mk-cert-bundle.pl path

--- pkgs/available/cacert.sh
+++ pkgs/available/cacert.sh
@@ -10,18 +10,17 @@ sudo apt-get -y install liblwp-useragent-determined-perl
 fi
 
 #curl -O http://curl.haxx.se/ca/cacert.pem
-git clone https://github.com/bagder/curl.git
-cd curl/lib
+git clone --depth=1 https://github.com/bagder/curl.git
+cd curl/scripts
 ./mk-ca-bundle.pl
 cp ca-bundle.crt $BUILD_DIR/cacert.pem
 
 cd $BUILD_DIR
 
 for target in $(find $VENV -name 'cacert.pem' | grep -v $VENV/build/cacert.pem); do
-cp cacert.pem $target
+  cp cacert.pem $target
 done
 
 for target in $(find $VENV -name 'cert.pem'); do
-cp cacert.pem $target
+  cp cacert.pem $target
 done
-

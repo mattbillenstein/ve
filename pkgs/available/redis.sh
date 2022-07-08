a0ecbb076c5c8664a556334a8ea256bbb052b835
Matt Billenstein <matt@vazor.com>
2022-07-08 09:56:55 +0000

    Fix redis sha

--- pkgs/available/redis.sh
+++ pkgs/available/redis.sh
@@ -1,5 +1,5 @@
 REDIS_VERSION="7.0.2"
-REDIS_SHA256SUM="8f0f6a738281b995fd698095a18065fd3fde5ee55c3229498a669ab21bdaf9cc"
+REDIS_SHA256SUM="5e57eafe7d4ac5ecb6a7d64d6b61db775616dbf903293b3fcc660716dbda5eeb"
 
 getpkg http://download.redis.io/releases/redis-${REDIS_VERSION}.tar.gz $REDIS_SHA256SUM
 tar zxf redis-${REDIS_VERSION}.tar.gz

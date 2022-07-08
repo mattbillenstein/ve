02fc518d194ae0395603f51fa8a2ad7043af3001
Matt Billenstein <matt@vazor.com>
2022-07-08 02:58:01 +0000

    Update squid

--- pkgs/available/squid.sh
+++ pkgs/available/squid.sh
@@ -1,12 +1,11 @@
-SQUID_VERSION="4.10"
-SQUID_SHA256SUM="df0685e851a1b68eede3790423986e30f4e162e9f7cd1cf92dbea65177201c8f"
+SQUID_VERSION="5.6"
+SQUID_SHA256SUM="5d11091c9e772c24bcf65cc7b11111493f78406e283d7139f34d3a43db785c06"
 
-getpkg http://www.squid-cache.org/Versions/v4/squid-${SQUID_VERSION}.tar.gz $SQUID_SHA256SUM
+getpkg http://www.squid-cache.org/Versions/v5/squid-${SQUID_VERSION}.tar.gz $SQUID_SHA256SUM
 tar zxf squid-${SQUID_VERSION}.tar.gz
 cd squid-${SQUID_VERSION}
 ./configure --prefix=$VENV --disable-auth --with-krb5-config=no --disable-external-acl-helpers --disable-eui --disable-arch-native --without-expat --without-libxml2 --without-gnutls --without-mit-krb5 --without-heimdal-krb5 --without-gnugss --without-netfilter-conntrack --without-libcap --without-nettle \
     --disable-strict-error-checking
-    # above fix re deprecated apis on macos 3/2020
 
 $PMAKE
 make install

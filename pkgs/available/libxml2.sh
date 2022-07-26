266ec59232d2179304a8abf8655a5178fb11f8d3
Matt Billenstein <matt@vazor.com>
2022-07-26 17:39:30 +0000

    Update libxml2

--- pkgs/available/libxml2.sh
+++ pkgs/available/libxml2.sh
@@ -1,8 +1,8 @@
-LIBXML2_VERSION="2.9.10"
-LIBXML2_SHA256SUM="aafee193ffb8fe0c82d4afef6ef91972cbaf5feea100edc2f262750611b4be1f"
+LIBXML2_VERSION="2.9.12"
+LIBXML2_SHA256SUM="c8d6681e38c56f172892c85ddc0852e1fd4b53b4209e7f4ebf17f7e2eae71d92"
 
 rm -fR libxml2*
-getpkg ftp://xmlsoft.org/libxml2/libxml2-${LIBXML2_VERSION}.tar.gz $LIBXML2_SHA256SUM
+getpkg http://xmlsoft.org/download/libxml2-${LIBXML2_VERSION}.tar.gz $LIBXML2_SHA256SUM
 tar zxf libxml2-${LIBXML2_VERSION}.tar.gz
 cd libxml2-${LIBXML2_VERSION}
 
@@ -16,7 +16,7 @@ LIBXSLT_VERSION="1.1.34"
 LIBXSLT_SHA256SUM="98b1bd46d6792925ad2dfe9a87452ea2adebf69dcb9919ffd55bf926a7f93f7f"
 
 rm -fR libxslt*
-getpkg ftp://xmlsoft.org/libxml2/libxslt-${LIBXSLT_VERSION}.tar.gz $LIBXSLT_SHA256SUM
+getpkg http://xmlsoft.org/download/libxslt-${LIBXSLT_VERSION}.tar.gz $LIBXSLT_SHA256SUM
 tar zxf libxslt-${LIBXSLT_VERSION}.tar.gz
 cd libxslt-${LIBXSLT_VERSION}
 

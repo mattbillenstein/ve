bf25582c17f004534f2b6bac565941e73ee2906a
Matt Billenstein <matt@vazor.com>
2023-10-10 17:04:06 -0700

    Fix libgcrypt on macos

--- pkgs/available/jemalloc.sh
+++ pkgs/available/jemalloc.sh
@@ -3,7 +3,7 @@ JEMALLOC_SHA256SUM="2db82d1e7119df3e71b7640219b6dfe84789bc0537983c3b7ac4f7189aec
 
 rm -fR ${JEMALLOC_VERSION}*
 getpkg https://github.com/jemalloc/jemalloc/releases/download/${JEMALLOC_VERSION}/jemalloc-${JEMALLOC_VERSION}.tar.bz2 $JEMALLOC_SHA256SUM
-tar jxvf jemalloc-${JEMALLOC_VERSION}.tar.bz2
+tar jxf jemalloc-${JEMALLOC_VERSION}.tar.bz2
 cd jemalloc-${JEMALLOC_VERSION}
 
 OPTS="--prefix=$VENV --disable-debug --disable-install-rdoc"
--- pkgs/available/libgcrypt.sh
+++ pkgs/available/libgcrypt.sh
@@ -3,8 +3,12 @@ LIBGPG_ERROR_SHA256SUM="9e3c670966b96ecc746c28c2c419541e3bcb787d1a73930f5e5f5e1b
 
 rm -fR libgpg-error*
 getpkg https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-${LIBGPG_ERROR_VERSION}.tar.bz2 $LIBGPG_ERROR_SHA256SUM
-tar jxvf libgpg-error-${LIBGPG_ERROR_VERSION}.tar.bz2
+tar jxf libgpg-error-${LIBGPG_ERROR_VERSION}.tar.bz2
 cd libgpg-error-${LIBGPG_ERROR_VERSION}
+
+./configure --prefix=$VENV
+$PMAKE
+make install
 
 cd $BUILD_DIR
 
@@ -13,9 +17,9 @@ LIBGCRYPT_SHA256SUM="3b9c02a004b68c256add99701de00b383accccf37177e0d6c58289664cc
 
 rm -fR libgcrypt*
 getpkg https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-${LIBGCRYPT_VERSION}.tar.bz2 $LIBGCRYPT_SHA256SUM
-tar jxvf libgcrypt-${LIBGCRYPT_VERSION}.tar.bz2
+tar jxf libgcrypt-${LIBGCRYPT_VERSION}.tar.bz2
 cd libgcrypt-${LIBGCRYPT_VERSION}
 
-./configure --prefix=$VENV
+./configure --prefix=$VENV --enable-random=linux # 10/2023 configure bug on macos...
 $PMAKE
 make install

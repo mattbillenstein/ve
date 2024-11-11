3994198f355fd76045162a3190cb9139b7642ba6
Matt Billenstein <matt@vazor.com>
2024-11-11 02:58:35 +0000

    Misc pkg updates

--- pkgs/available/ruby.sh
+++ pkgs/available/ruby.sh
@@ -1,5 +1,5 @@
-RUBY_VERSION="3.2.2"
-RUBY_SHA256SUM="96c57558871a6748de5bc9f274e93f4b5aad06cd8f37befa0e8d94e7b8a423bc"
+RUBY_VERSION="3.3.6"
+RUBY_SHA256SUM="8dc48fffaf270f86f1019053f28e51e4da4cce32a36760a0603a9aee67d7fd8d"
 
 getpkg https://cache.ruby-lang.org/pub/ruby/${RUBY_VERSION%.*}/ruby-${RUBY_VERSION}.tar.gz $RUBY_SHA256SUM
 tar zxf ruby-${RUBY_VERSION}.tar.gz
--- pkgs/available/zlib.sh
+++ pkgs/available/zlib.sh
@@ -1,5 +1,5 @@
-ZLIB_VERSION="1.3"
-ZLIB_SHA256SUM="ff0ba4c292013dbc27530b3a81e1f9a813cd39de01ca5e0f8bf355702efa593e"
+ZLIB_VERSION="1.3.1"
+ZLIB_SHA256SUM="9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23"
 
 rm -fR zlib*
 getpkg https://www.zlib.net/zlib-${ZLIB_VERSION}.tar.gz $ZLIB_SHA256SUM
